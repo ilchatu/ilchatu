89b0fe7e441ac11fd10ee8828155ead5da171195
ilchatu <152178314+ilchatu@users.noreply.github.com>
2023-11-30 04:55:40 +0800

    Update SingleChat.js

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -19,7 +19,7 @@ import ScrollableChat from "./ScrollableChat";
 
 import io from "socket.io-client";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://ilchatu.onrender.com";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
