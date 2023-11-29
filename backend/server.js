b2fe28beb11da6ce9f1188744d950b234b19da2c
ilchatu <152178314+ilchatu@users.noreply.github.com>
2023-11-30 04:54:53 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://ilchatu.onrender.com",
   },
 });
 
